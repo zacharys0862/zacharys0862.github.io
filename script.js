7176eee15769e7ad836ed0b0441e2309b242e38c
zacharys0862 <91750452+zacharys0862@users.noreply.github.com>
2021-10-01 13:06:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Zachary S"; // update the part BETWEEN the "quotes"
+let userName = "zacharys0862"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
